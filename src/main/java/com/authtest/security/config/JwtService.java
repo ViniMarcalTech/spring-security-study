d94793723044b68c0fba941ab11e08b34c664265
ViniMarcal.tech <vinicius.dm1515@gmail.com>
2024-04-16 13:04:12 -0300

    JwtService update

--- src/main/java/com/authtest/security/config/JwtService.java
+++ src/main/java/com/authtest/security/config/JwtService.java
@@ -1,12 +1,84 @@
 package com.authtest.security.config;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
 @Service
+@RequiredArgsConstructor
 public class JwtService {
 
+    private static SecretKeyGenerator keyGenerator;
+
+
     public String extractUsername(String token) {
-        return null;
+        return extractClaim(token,Claims::getSubject);
 
     }
+
+
+    public <T> T extractClaim(String token, Function<Claims,T> claimsResolver){
+        final Claims claims = exctractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Claims exctractAllClaims(String token) {
+        return Jwts.parser()
+                .verifyWith(keyGenerator.getKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+
+
+    private Key getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(keyGenerator.getKey());
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public String generateToken(UserDetails userDetails){
+        return generateToken(new HashMap<>(), userDetails);
+    }
+
+    public String generateToken(Map<String, Object> extraClaims,
+                                UserDetails userDetails){
+
+        Date creationDate = new Date(System.currentTimeMillis());
+        Long expirationMinutes = 10L;
+
+        return Jwts.builder()
+                .claims().add(extraClaims).and()
+                .subject(userDetails.getUsername())
+                .issuedAt(creationDate)
+                .expiration(creationDate.getTime() + (1000L * 60L * expirationMinutes))
+                .signWith(getSignInKey(), SignatureAlgorithm.ES256)
+                .compact();
+    }
+
+    public boolean isTokenValid(String token, UserDetails userDetails){
+        final String username = extractUsername(token);
+        return username.equals(userDetails.getUsername()) && !isTokenExpired(token);
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token) {
+        extractClaim(token,Claims::getExpiration);
+
+    }
+
+
 }
