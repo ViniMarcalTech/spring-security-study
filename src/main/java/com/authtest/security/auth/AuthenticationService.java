34f460f12460abbb0fd30d854907d205c1be250a
ViniMarcal.tech <vinicius.dm1515@gmail.com>
2024-04-17 09:44:49 -0300

    AuthenticationService - authenticate_method

--- src/main/java/com/authtest/security/auth/AuthenticationService.java
+++ src/main/java/com/authtest/security/auth/AuthenticationService.java
@@ -5,6 +5,8 @@ import com.authtest.security.user.Role;
 import com.authtest.security.user.User;
 import com.authtest.security.user.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -15,6 +17,7 @@ public class AuthenticationService {
     private final UserRepository repository;
     private final PasswordEncoder passwordEncoder;
     private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
 
     public AuthenticationResponse register(RegisterRequest request) {
 
@@ -36,6 +39,14 @@ public class AuthenticationService {
 
     public AuthenticationResponse authenticate(AuthenticationRequest request) {
 
-        return null;
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(request.getEmail(),request.getPassword())
+        )
+
+        var user = repository.findByEmail(request.getEmail()).orElseThrow();
+
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken).build();
     }
 }
